0c69b950c8f489bd2f2d81f83c9569e14312aedb
Azambik <71612017+Azambik@users.noreply.github.com>
2020-11-07 17:37:28 -0500

    add stat persistence

--- assets/js/script.js
+++ assets/js/script.js
@@ -245,6 +245,21 @@ var completeEditTask = function(taskName, taskType, taskId) {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   };
 
+  var loadTasks = function() {
+    var savedTasks = localStorage.getItem("tasks");
+  
+    if (!savedTasks) {
+      return false;
+    }
+  
+    savedTasks = JSON.parse(savedTasks);
+    // loop through savedTasks array
+for (var i = 0; i < savedTasks.length; i++) {
+  // pass each task object into the `createTaskEl()` function
+  createTaskEl(savedTasks[i]);
+}
+  };
+  loadTasks();
   //event listeners
   formEl.addEventListener("submit", taskFormHandler);
   pageContentEl.addEventListener("change", taskStatusChangeHandler);
